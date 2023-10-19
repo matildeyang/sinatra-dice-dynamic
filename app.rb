3910df38b4555226a402b557b16923b7926f07c5
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-19 03:27:06 +0000

    how mant sides

--- app.rb
+++ app.rb
@@ -53,11 +53,15 @@ get("/dice/5/4") do
   erb(:five_four)
 end
 
-get("/dynamic/:number_of_dice/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
   @rolls = []
+  # use the fetch method to get what the user inputs 
+  # need to convert it to integer because it comes as a string
+  @dice_num = params.fetch("number_of_dice").to_i
+  @side_num = params.fetch("how_many_sides").to_i
 
-  50.times do
-    die = rand(1..6)
+  @dice_num.times do
+    die = rand(1..@side_num)
 
     @rolls.push(die)
   end
